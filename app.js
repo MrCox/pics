33c4f5c97558f04f213016705c6e974d28c94bcd
Danny_C <danny.cox@aunalytics.com>
2013-10-06 15:18:56 -0400

    Adding chokidar

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ var express = require('express'),
   http = require('http'),
   mongoose = require('mongoose'),
   db = mongoose.connect('mongodb://localhost/comics'),
-  schema = mongoose.Schema;
+  schema = mongoose.Schema,
+  dir = require('chokidar'),
+  path = '/root/pics/public/comics/';
+
+var watcher = dir.watch(path, {ignored:/^\./, persistent: true});
+
+watcher.on('add', function(p){
+  console.log(path, stats)
+});
 
 var comic = new schema({
   title: String,
@@ -14,21 +22,21 @@ var comic = new schema({
 
 var c = db.model('comic', comic);
 
-c.find({'title':'test'}, function(err, docs){
-});
+//c.find({'title':'test'}, function(err, docs){
+//});
 
-app.configure( function() {
+app.configure(function() {
   app.set('port', process.env.PORT || 80);
   app.set('views','./views');
   app.set('views','./views');
   app.set('view engine', 'jade');
   app.use(app.router);
   app.use('/public', express.static(__dirname+'/public'));
-})
+});
 app.get('/', function(req, res){
   res.render('layout.jade', {pageTitle:'test'});
-})
+});
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('listening on port ' + app.get('port'));
-})
+});
